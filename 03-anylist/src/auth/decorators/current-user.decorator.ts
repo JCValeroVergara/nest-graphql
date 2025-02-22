caaefbd5760d9a15eddfa147fce8c8220768f3b8
Juank_Valero <jvalero2009@gmail.com>
2025-02-21 23:26:58 -0500

    feat: enhance current user decorator to support role-based access control; add ValidRoles enum

--- 03-anylist/src/auth/decorators/current-user.decorator.ts
+++ 03-anylist/src/auth/decorators/current-user.decorator.ts
@@ -1,17 +1,25 @@
-import { createParamDecorator, ExecutionContext, InternalServerErrorException } from '@nestjs/common';
+import { createParamDecorator, ExecutionContext, ForbiddenException, InternalServerErrorException } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
+import { ValidRoles } from '../enums';
+import { User } from 'src/users/entities';
 
 
 export const CurrentUser = createParamDecorator(
-    (roles = [], context: ExecutionContext) => {
+    (roles: ValidRoles[] = [], context: ExecutionContext) => {
         
         const ctx = GqlExecutionContext.create(context);
-        const user = ctx.getContext().req.user;
+        const user: User = ctx.getContext().req.user;
 
         if( !user){
             throw new InternalServerErrorException(`No user found in request context - make sure that we used the AuthGuard`);
         }
 
-        return user;
+        if (roles.length === 0) return user;
+        
+        for (const role of user.roles) {
+            if (roles.includes(role as ValidRoles)) return user;
+        }
+
+        throw new ForbiddenException(`User ${user.fullName} does not have the necessary roles to access this resource`);
     }
 );
